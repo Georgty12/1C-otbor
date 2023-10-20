2a66e9ff1d67397404708daac5e66370d301a063
Kiriia Georgii <kiriia.gb@phystech.edu>
2023-10-20 23:17:07 +0300

    fix

--- __main__.py
+++ __main__.py
@@ -1,23 +1,40 @@
 import os
 from difflib import SequenceMatcher
-
+import Levenstein
 dir1_name = "dir1"    
 dir2_name = "dir2"
 
-if __name__ == "__main__":    
+def read_directories():
     dir1_path = os.getcwd() + "/1C-otbor/" + dir1_name
     dir2_path = os.getcwd() + "/1C-otbor/" + dir2_name
     _, _, files1_names = next(os.walk("{}".format(dir1_path)))
     _, _, files2_names = next(os.walk("{}".format(dir2_path)))
-    
+
     files_count1 = len(files1_names)
     files_count2 = len(files2_names)
-    
+
     files1 = [open("{dir_path}/{file_name}".format(dir_path=dir1_path, file_name=files1_names[i])).read() for i in range(files_count1)]
     files2 = [open("{dir_path}/{file_name}".format(dir_path=dir2_path, file_name=files2_names[i])).read() for i in range(files_count2)]
-    
+
+    return files1, files2, files1_names, files2_names
+
+def remove_from_unique(file1, file2, unique_files1, unique_files2):
+    if (file1 in unique_files1):
+        unique_files1.remove(file1)
+
+    if (file2 in unique_files2):
+        unique_files2.remove(file2)
+
+if __name__ == "__main__":    
+
+    files1, files2, files1_names, files2_names = read_directories()
+
+    files_count1 = len(files1)
+    files_count2 = len(files2)
+
     percentage = int(input("Write the percentage number like \"33\"\n"))
     target_accuracy = percentage / 100
+
     same_files = []
     similar_files = []
     unique_files1 = set(files1_names)
@@ -37,16 +54,11 @@ if __name__ == "__main__":
             accuracy_between_files = matches / size1
             if accuracy_between_files == 1:
                     same_files.append([files1_names[i], files2_names[j]])
-                    if (files1_names[i] in unique_files1):
-                        unique_files1.remove(files1_names[i])
-                    if (files2_names[j] in unique_files2):
-                        unique_files2.remove(files2_names[j])
+                    remove_from_unique(files1_names[i], files2_names[j], unique_files1, unique_files2)
+
             elif accuracy_between_files >= target_accuracy:
                     similar_files.append([files1_names[i], files2_names[j]])
-                    if (files1_names[i] in unique_files1):
-                        unique_files1.remove(files1_names[i])
-                    if (files2_names[j] in unique_files2):
-                        unique_files2.remove(files2_names[j])      
+                    remove_from_unique(files1_names[i], files2_names[j], unique_files1, unique_files2)
 
     print("same files are:")
     for i in range(len(same_files)):
@@ -66,10 +78,10 @@ if __name__ == "__main__":
     print("\nunique files for dir1 are:")
     for name_file in unique_files1:
         print(
-              "{dir_path}/{file_name}".format(dir_path=dir1_name, file_name=name_file)
+              "{file_name}".format(file_name=name_file)
         )
     print("\nunique files for dir2 are:")
     for name_file in unique_files2:
         print(
-              "{dir_path}/{file_name}".format(dir_path=dir2_name, file_name=name_file)
+              "{file_name}".format(file_name=name_file)
         )
